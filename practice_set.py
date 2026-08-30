15c450c0b8035c85f27d8e3911bd4bb0ca6c6a50
zoharafatima <zoharaf182@gmail.com>
2026-08-30 17:17:14 +0530

    Practise set

---
+++ practice_set.py
@@ -0,0 +1,26 @@
+#this is the first question
+# name=input("Enter your name: ")
+# print("Good morning" +" "+ name)
+#---------------------------------------------------
+#question 2
+# from datetime import datetime
+# name=input("Enter your name: ")
+# date=datetime.now()
+# print(f'''Dear {name},
+#       You are selected!
+#       Date: {date}
+#       ''')
+#---------------------------------------------------
+#question 3
+# text=input("Enter your text: ")
+# count=0
+# if "  " in text:
+#     print("double space found")
+# else:
+#     print("double space not found")
+
+#---------------------------------------------------
+#question 4
+text=input("Enter your text: ")
+text=text.replace("  "," ")
+print(text)
